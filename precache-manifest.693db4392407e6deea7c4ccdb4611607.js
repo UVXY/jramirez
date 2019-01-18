34c9a52874caa79e8937430602046cef5b5316fe
ricky <ricky@rickys-MacBook-Air.local>
2019-01-17 19:42:16 -0800

    Updates

---
+++ precache-manifest.693db4392407e6deea7c4ccdb4611607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jramirez/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0840ced17deecc75027f",
+    "url": "/jramirez/static/js/runtime~main.0840ced1.js"
+  },
+  {
+    "revision": "a7cbabdfbca99d2e7397",
+    "url": "/jramirez/static/js/main.a7cbabdf.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/jramirez/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a7cbabdfbca99d2e7397",
+    "url": "/jramirez/static/css/main.2c61ba22.chunk.css"
+  },
+  {
+    "revision": "685f4ae8f34b7ce55760c617723f1213",
+    "url": "/jramirez/index.html"
+  }
+];
