cd29f7194358fbc66eedc35801aaaa72f9f2db94
ricky <ricky@rickys-MacBook-Air.local>
2019-01-31 23:35:43 -0800

    Updates

---
+++ precache-manifest.9b68afb8bb03c3619ded6987903cd229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "841b402b235fca15c77f11cfb4edf028",
+    "url": "/static/media/logo.841b402b.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1264b4257bd1002dfb2",
+    "url": "/static/js/main.a1264b42.chunk.js"
+  },
+  {
+    "revision": "04f3b7291ada81a61c52",
+    "url": "/static/js/1.04f3b729.chunk.js"
+  },
+  {
+    "revision": "a1264b4257bd1002dfb2",
+    "url": "/static/css/main.3275a106.chunk.css"
+  },
+  {
+    "revision": "9cb1a107fbe205264d867f03375406e8",
+    "url": "/index.html"
+  }
+];
